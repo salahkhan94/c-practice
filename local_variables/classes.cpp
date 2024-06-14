7ce08ce96998270c7b58d4db201fa7ea81a5d5b7
Salahuddin Khan <salahuddin.khan.1994@gmail.com>
2024-06-13 23:38:04 -0400

    namespaces & function pointers

--- local_variables/classes.cpp
+++ local_variables/classes.cpp
@@ -15,7 +15,38 @@ class URL {
         }
 };
 
+int x{23};
+
+namespace abc {
+    int x{47};
+    void func() {
+        cout << "x = " << x << endl;
+        cout << "::x = " << ::x << endl;
+    }
+}
+
+void func(int a, int b) {
+    cout << a << " + " << b << " = " << a+b << endl;
+}
+using pfunc = void (*)(int, int);
+
+void some_func(int a, int b, pfunc func_ptr) {
+    (*func_ptr)(a, b);
+}
+
+pfunc other_func(){
+    return &func;
+}
+
 int main() {
     URL url("http", "www.example.com/index.html");
     url.display();
+    abc::func();
+
+    auto func_ptr = &func;
+    (*func_ptr)(2, 4);
+
+    auto func_p = other_func();
+
+    some_func(2, 4, func_p);
 }
