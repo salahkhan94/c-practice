c54d6355d158093abdc2c88078d745c14bf70421
Salahuddin Khan <salahuddin.khan.1994@gmail.com>
2025-01-08 09:55:57 -0500

    stack

--- leetcode/valid_parenthesis.cpp
+++ leetcode/valid_parenthesis.cpp
@@ -5,6 +5,49 @@ using namespace std;
 class Solution {
 public:
     bool isValid(string s) {
-        
+        unordered_map<char, char> bracket_map = {
+            {')', '('}, 
+            {']', '['}, 
+            {'}', '{'}
+            };
+        vector<char> stack;
+
+        for (auto & chr : s) {
+            if (chr == '('|| chr == '[' || chr == '{') stack.push_back(chr);
+
+            else {
+                if (stack.empty() || stack.back() != bracket_map[chr]) {
+                    return false;
+                }
+                stack.pop_back();
+            }
+        }
+        return stack.empty();
+
+    }
+};
+
+class Solution {
+public:
+    bool isValid(string s) {
+        unordered_map<char, char> bracket_map = {
+            {')', '('}, 
+            {']', '['}, 
+            {'}', '{'} 
+        };
+        stack<char> stck;
+
+        for (auto& ch : s) {
+            if (ch=='('|| ch == '[' || ch == '{') {
+                stck.push(ch);
+            }
+            else {
+                if (stck.empty() || stck.top() != bracket_map[ch]) {
+                    return false;
+                }
+                stck.pop();
+            }
+        }
+        return stck.empty();
     }
 };
