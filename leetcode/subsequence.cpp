329458d382824050f7cebb4f5fced0ae748e9982
Salahuddin Khan <salahuddin.khan.1994@gmail.com>
2025-06-22 22:49:35 -0400

    Second round

--- leetcode/subsequence.cpp
+++ leetcode/subsequence.cpp
@@ -15,3 +15,17 @@ public:
         return s_ptr == s.size();
     }
 };
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        u_int s_ptr = 0;
+        u_int t_ptr = 0;
+
+        while(s_ptr < s.size() && t_ptr < t.size()) {
+            if (s[s_ptr] == t[t_ptr]) s_ptr++;
+            t_ptr++;
+        }
+        return s_ptr==s.size();
+    }
+};
