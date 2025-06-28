7eb66c4be3b55e2b8dd427ca16bf88b48d1bb397
Salahuddin Khan <salahuddin.khan.1994@gmail.com>
2025-06-27 23:37:24 -0400

    Second round

--- leetcode/spiral_matrix.cpp
+++ leetcode/spiral_matrix.cpp
@@ -45,3 +45,46 @@ public:
         return spiral;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> spiral;
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        int top = 0;
+        int bottom = rows-1;
+
+        int left = 0;
+        int right = cols-1;
+
+        while(left<=right && top <= bottom) {
+            for (int i = left; i < right+1; i++) {
+                spiral.push_back(matrix[top][i]);
+            }
+            top++;
+
+            for (int i = top; i < bottom+1; i++) {
+                spiral.push_back(matrix[i][right]);
+            }
+            right--;
+
+            if (left<=right && top <= bottom) {
+                for(int i = right; i > left-1; i--) {
+                    spiral.push_back(matrix[bottom][i]);
+                }
+            }
+            bottom--;
+            if (left<=right && top <= bottom) {
+                for(int i = bottom; i > top-1; i--) {
+                    spiral.push_back(matrix[i][left]);
+                }
+            }
+            left++;
+        }
+        return spiral;
+    }
+};
