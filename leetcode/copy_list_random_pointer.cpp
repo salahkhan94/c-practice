e150f367aa1b3e4159618a2518a5d8ef660780ac
Salahuddin Khan <salahuddin.khan.1994@gmail.com>
2025-07-17 23:30:24 -0400

    2nd round

--- leetcode/copy_list_random_pointer.cpp
+++ leetcode/copy_list_random_pointer.cpp
@@ -54,3 +54,43 @@ public:
         return head_copy;
     }
 };
+
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (!head)
+            return NULL;
+        Node * current = head;
+        while(current) {
+            Node* newnode = new Node(current->val);
+            newnode->next = current->next;
+            current->next = newnode;
+            current = newnode->next;
+        }
+        current = head;
+        while(current) {
+            Node * cur_copy = current->next;
+            Node *rand = current->random;
+            if(rand) {
+                cur_copy->random = rand->next;
+            }
+            else {
+                cur_copy->random = rand;
+            }
+
+            current = cur_copy->next;
+        }
+        current = head;
+        Node* head_copy = head->next;
+        while(current) {
+            Node * copy = current->next;
+            current->next = copy->next;
+            if (current->next) {
+            copy->next = current->next->next;
+            }
+            current = current->next;
+        }
+        return head_copy;
+    }
+};
