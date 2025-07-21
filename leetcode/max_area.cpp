6cbef124f16160eba87cbfa220c691df7f0c9ec7
Salahuddin Khan <salahuddin.khan.1994@gmail.com>
2025-07-20 23:50:05 -0400

    2nd round

--- leetcode/max_area.cpp
+++ leetcode/max_area.cpp
@@ -21,3 +21,23 @@ public:
         return maxarea;
     }
 };
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() -1;
+
+        int maxarea = 0;
+
+        while (left < right) {
+            int cwidth = right-left;
+            int cheight = min(height[right], height[left]);
+            maxarea  = max(maxarea, cwidth * cheight);
+
+            if(height[right] > height[left]) left += 1;
+            else right -= 1;
+        }
+        return maxarea;
+    }
+};
