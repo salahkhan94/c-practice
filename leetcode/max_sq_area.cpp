0762228c75a364e4f08427c10dad7093f46aed2b
Salahuddin Khan <salahuddin.khan.1994@gmail.com>
2026-01-18 23:06:42 -0500

    3rd round

--- leetcode/max_sq_area.cpp
+++ leetcode/max_sq_area.cpp
@@ -4,14 +4,24 @@ using namespace std;
 
 class Solution {
     public:
-        int max_area(vector<vector<int>>& land_area) {
+        int max_area(vector<vector<int>>& land) {
             int area_max;
-            for (int i = 0; i < land_area.size(); i++) {
-                for (int j = 0; j < land_area[0].size(); j++) {
-                    if (land_area[i][j] == 1) {
+            for (int i = 0; i < land.size(); i++) {
+                for (int j = 0; j < land[0].size(); j++) {
+                    if (land[i][j] == 1) {
                         area_max = 1;
-                        int col = j+1;
-                        
+                        int increment = 0;
+                        int col;
+                        int row;
+                        while(land[row][col]) {
+                            for (row = i; row < i + increment; row++) {
+                                
+                            }
+                            while() {
+
+                            }
+                        }
+
                     }
                 }
             }
