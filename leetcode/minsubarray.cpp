6554c22872124ccd55209547f369229ce2f89859
Salahuddin Khan <salahuddin.khan.1994@gmail.com>
2026-01-25 23:42:23 -0500

    3rd round

--- leetcode/minsubarray.cpp
+++ leetcode/minsubarray.cpp
@@ -61,3 +61,23 @@ public:
 
     }
 };
+
+
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int min_sub = nums.size() + 1;
+        int sum = 0;
+        int left = 0;
+        for (int right = 0; right < nums.size(); right++) {
+            sum += nums[right];
+            while(sum >= target) {
+                min_sub = min(min_sub, right-left+1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+        return (min_sub < (nums.size()+1)) ? min_sub : 0;
+
+    }
+};
